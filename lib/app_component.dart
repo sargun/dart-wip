24e062d3be452fa215a8eb8bcf9cbe6ab04283d8
Sargun Dhillon <sargun@sargun.me>
2017-01-09 13:50:40 -0800

    Initial Commit

---
+++ lib/app_component.dart
@@ -0,0 +1,30 @@
+// Copyright (c) 2017, sdhillon. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular2/core.dart';
+import 'package:angular2_components/angular2_components.dart';
+
+import 'package:ringoftrust_web/hello_dialog/hello_dialog.dart';
+
+@Component(
+  selector: 'my-app',
+  styleUrls: const ['app_component.css'],
+  templateUrl: 'app_component.html',
+  directives: const [materialDirectives, HelloDialog],
+  providers: const [materialProviders],
+)
+class AppComponent {
+  // Nothing here yet. All logic is in HelloDialog.
+  List<String> heroes = buildNames().toList();
+}
+
+Iterable<String> buildNames() sync* {
+  var prefixes = ["Super", "Mega", "Ultra"];
+  var suffixes = ["man", "woman", "boy", "girl"];
+
+  for (var prefix in prefixes) {
+    for (var suffix in suffixes) {
+      yield "$prefix$suffix";
+    }
+  }
+}
